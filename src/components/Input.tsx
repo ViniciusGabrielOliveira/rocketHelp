1e25ce3a8e971ad023b47d846d023869bf1185da
ViniciusGabrielOliveira <viniciusgabriel.oliveira@gmail.com>
2022-10-20 20:14:18 -0300

    create app rockthelp

---
+++ src/components/Input.tsx
@@ -0,0 +1,25 @@
+import { IInputProps, Input as NativeBaseInput } from 'native-base';
+
+export function Input({ ...rest }: IInputProps) {
+  return (
+
+    <NativeBaseInput
+        bg="gray.700"
+        h={14}
+        size="md"
+        borderWidth={0}
+        fontSize="md"
+        color="white"
+        placeholderTextColor="gray.300"
+        _focus={{
+            borderWidth: 1,
+            borderColor: "gray.500",
+            bg: "gray.700"
+        }
+
+        }
+        { ...rest }
+    />
+
+  );
+}
